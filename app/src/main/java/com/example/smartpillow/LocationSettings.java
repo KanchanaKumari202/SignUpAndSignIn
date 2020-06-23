e0a88478cb6c4595a685e2121600a8dac91d6be7
Kanchi <ckkumari202@gmail.com>
2020-06-23 20:59:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smartpillow/LocationSettings.java
@@ -0,0 +1,14 @@
+package com.example.smartpillow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_settings);
+    }
+}
